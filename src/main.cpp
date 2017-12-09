b4ea0486ee480a5c9db7767a1de4538e7d7f517e
Essenceia <julia.desmazes@gmail.com>
2017-12-09 23:05:04 +0100

    Why not workig ?

--- src/main.cpp
+++ src/main.cpp
@@ -2,28 +2,56 @@
 // Created by pookie on 08/12/17.
 //
 
-#include "Arduino.h"
-typedef struct{
+#include <Arduino.h>
+/*
+typedef struct {
     u_int32_t c1;
     u_int32_t c2;
-    float f1,f2;
-}sensor_data;
+    float f1, f2;
+} sensor_data;
 
-void write_serial(sensor_data *data){
-    Serial.print(data->c1);
-    Serial.print(data->c2);
-    Serial.print(data->f1);
-    Serial.print(data->f2);
-}
-int main(){
-    sensor_data data = { .c1 = 1, .c2 = 2 , .f1 =3 , .f2 =4};
+void write_serial(u_int8_t *data) {
+
+    for (u_int8_t i = 0; i < 17; i++) {
+        Serial.print(data[i]);
+    }
+    Serial.print('\0');
+}*/
+
+int main() {
+    pinMode(13, OUTPUT);
+    /*u_int8_t dmsg[17] = {1,
+                         1, 1, 2, 1,
+                         1, 1, 10, 2,
+                         0x40, 0xdf, 0xae, 0x14,
+                         0x3f, 0xc7, 0xae, 0x14
+    };*/
+    digitalWrite(13, 1);
+
+
     Serial.begin(9600);
-    while(true) {
+
+    delay(2000);
+
+    while (true) {
         /*  check if data has been sent from the computer: */
-        if (Serial.available()) {
-            delay(2000);
-            write_serial(&data);
-        }
+
+        Serial.println("SUUS");
+
+
+        digitalWrite(13, 0);
+
+        delay(50);
+
+
+        Serial.println("D");
+
+
+        digitalWrite(13, 1);
+
+        delay(50);
+
+
     }
     return 0;
 }
